094fffbabe878f930a3c225ae12e31ab85a1ae2f
simon <simondaniel@gmail.com>
2018-08-30 01:49:02 +0200

    added class to comfortable change color of strings in stage2d

---
+++ PokemonGDX/core/src/net/simondaniel/MyColor.java
@@ -0,0 +1,11 @@
+package net.simondaniel;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class MyColor {
+
+	
+	public static String dye(Color c, String s) {
+		return "[#" + c + "]" + s + "[]";
+	}
+}
